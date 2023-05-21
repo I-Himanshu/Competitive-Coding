8ca8404d7596843fe2a7d7def0bd3127dc664923
Himanshu kumar <83366556+I-Himanshu@users.noreply.github.com>
2023-05-21 19:25:13 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (89.05%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        int c0=0,c1=0,c2=0;
         
-        for(int i = 0;i<nums.size()-1;i++){
-            for(int j = i;j<nums.size();j++){
-                if(nums[i]>nums[j]){
-                    swap(nums[i],nums[j]);
-                }
+        for(auto num:nums){
+            if(num==0) c0++;
+            else if(num==1) c1++;
+            else c2++;
+        }
+        
+        for(int i = 0;i<nums.size();i++){
+            if(c0){
+                nums[i]=0;
+                c0--;
+            }else if(c1){
+                nums[i]=1;
+                c1--;
+            }else{ 
+                nums[i]=2;
             }
         }
     }
